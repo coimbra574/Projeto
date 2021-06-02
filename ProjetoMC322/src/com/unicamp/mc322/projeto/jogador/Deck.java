6ea5fd5b5447696d895f7e445fa966c94557632b
andreEidi <andre_eidi_maeda@outlook.com>
2021-06-02 18:05:02 -0300

    Implementção deck, poucas alterações na classe jogador

---
+++ ProjetoMC322/src/com/unicamp/mc322/projeto/jogador/Deck.java
@@ -0,0 +1,66 @@
+package com.unicamp.mc322.projeto.jogador;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+	private int numCartasDeck = 40;
+	private ArrayList<Carta> baralho = new ArrayList<Carta>();
+	private String tipoDeck;//Não sei se é necessário, acho q depende da nossa implementação se vamos usar mais cartas do q as recomendadas e tals
+	
+	
+	Deck(String tipoDeck) {
+		super();
+		this.tipoDeck = tipoDeck;
+		contruirDeck(;)//deckFactory
+	}
+	
+	public void contruirDeck() {
+		//por enquanto vou deixar como stub, pois não sei como fazer isso de deck factory. Além de precisar das cartas para contruir a carta
+	}
+	
+	ArrayList<Carta> obterCartasIniciais(){
+		ArrayList<Carta> mao;
+		for(int i=0;i<4;i++) {
+			Carta carta = baralho.get(i);
+			mao.add(carta);
+		}
+		for(int i=0;i<4;i++) {
+			Carta carta = baralho.get(i);
+			baralho.remove(carta);
+			numCartasDeck-=1;
+		}
+	}
+	
+	Carta pegarCartaAleatoriaDeck() {
+		int numero;
+		Carta carta;
+		
+		Random geradorAleatorio = new Random();
+		numero = geradorAleatorio.nextInt(numCartasDeck);
+		carta = baralho.get(numero);
+		baralho.remove(carta);
+		numCartasDeck-=1;
+		return carta;
+	}
+	
+	void recolocarNoBaralho(Carta carta) {
+		int numero;
+		Carta carta;
+		
+		Random geradorAleatorio = new Random();
+		numero = geradorAleatorio.nextInt(numCartasDeck);
+		baralho.add(numero, carta);
+		numCartasDeck+=1;
+	}
+	
+	Carta obterCartaDeck() {
+		Carta carta;
+		
+		carta = baralho.get(0);
+		baralho.remove(carta);
+		numCartasDeck-=1;
+		return carta;
+	}
+
+}
