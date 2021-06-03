259ce1a7430e96ecefb770739fc82c51bbe79d20
LeonardoRener <leonardo-rener@hotmail.com>
2021-06-03 18:49:22 -0300

    Classes de Interface e Classe Game
    Adicionada as classes InterfaceGraficaCampo, InterfaceTerminal, RunneteraGame e o enum ModoDeJogo.

---
+++ ProjetoMC322/src/com/unicamp/mc322/projeto/Interface/InterfaceTerminal.java
@@ -0,0 +1,97 @@
+package com.unicamp.mc322.projeto.Interface;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.unicamp.mc322.projeto.ModoDeJogo;
+import com.unicamp.mc322.projeto.jogador.Deck;
+
+public class InterfaceTerminal {
+	private boolean exitSelected = false;
+	private Scanner keyboard = new Scanner(System.in);
+	
+	public void GameStart() {
+		System.out.println("Bem Vindo ao Jogo!");
+	}
+	
+	public boolean getExitSelected() {
+		return exitSelected;
+	}
+	
+	public ModoDeJogo escolherModo() {
+		while(true) {
+			System.out.println("Escolha um modo de jogo:");
+			System.out.println("Digite [1] - Humano vs Humano");
+			System.out.println("Digite [2] - Humano vs Computador");
+			System.out.println("Digite [3] - Computador vs Computador");
+			String comando = keyboard.nextLine();
+			if(comando == "1") {
+				return ModoDeJogo.HUMANOXHUMANO;
+			}
+			else if(comando == "2") {
+				return ModoDeJogo.HUMANOXCOMPUTADOR;
+			}
+			else if(comando == "3") {
+				return ModoDeJogo.COMPUTADORXCOMPUTADOR;
+			}
+			else {
+				System.out.println("Entrada Invalida!");
+			}
+		}
+	}
+
+	public void exitGame() {
+		System.out.println("Fim de Jogo!");
+		
+	}
+
+	public Deck escolherDeck(String nomePlayer, Deck[] listaDecks) {
+		while(true) {
+			System.out.printf("Escolha um deck para o %s\n", nomePlayer);
+			for(int i = 0; i < listaDecks.length; i++) {
+				System.out.printf("Digite [%d] - Para o Deck:\n", i+1);
+				System.out.println(listaDecks[i].toString());
+				System.out.println();
+			}
+			String comando = keyboard.nextLine();
+			if(Integer.parseInt(comando) >= 1 && Integer.parseInt(comando) <= listaDecks.length) {
+				return listaDecks[Integer.parseInt(comando)-1];
+			}
+		}
+	}
+	
+	public ArrayList<Carta> substituirCartasIniciais(ArrayList<Carta> listaCartas) {
+		ArrayList<Integer> comandosDigitados;
+		ArrayList<Carta> listaDeSubstituicao;
+		String comando;
+		
+		System.out.println("Deseja substituir algumas das seguintes cartas iniciais?");
+		for(int i = 0; i < listaCartas.length; i++) {
+			System.out.printf("Digite [%d] - Para substituir a carta:", i+1);
+			System.out.println(listaCartas.get(i).ToString());
+		}
+		do {
+			System.out.println("Digite um numero ou [OK] para confirmar:");
+			comando = keyboard.nextLine();
+			if(Integer.parseInt(comando) >= 1 && Integer.parseInt(comando) <= listaCartas.length) {
+				if(comandosDigitados.contains(Integer.parseInt(comando)-1)) {
+					comandosDigitados.remove(Integer.parseInt(comando)-1);
+					System.out.printf("Carta [%d] nao sera trocada.\n", Integer.parseInt(comando));
+				}
+				else {
+					comandosDigitados.add(Integer.parseInt(comando)-1, Integer.parseInt(comando)-1);
+					System.out.printf("Carta [%d] sera trocada.\n", Integer.parseInt(comando));
+				}
+			}
+			else if(!comando.equals("OK")){
+				System.out.println("Entrada Invalida!");
+			}
+		}while(!comando.equals("OK"));
+		
+		for(int i: comandosDigitados) {
+			listaDeSubstituicao.add(listaCartas.get(i));
+		}
+		return listaDeSubstituicao;
+	}
+
+}
---
+++ ProjetoMC322/src/com/unicamp/mc322/projeto/RunneteraGame.java
@@ -0,0 +1,53 @@
+package com.unicamp.mc322.projeto;
+
+import com.unicamp.mc322.projeto.Interface.*;
+import com.unicamp.mc322.projeto.jogador.*;
+
+public class RunneteraGame {
+	private boolean exitSelected = false;
+	private InterfaceTerminal interfaceTerminal = new InterfaceTerminal();
+	private ModoDeJogo modoDeJogo;
+	private Deck[] listaDecks;
+	private Jogador player1;
+	private Jogador player2;
+	
+	public RunneteraGame(Deck[] listaDecks) {
+		this.listaDecks = listaDecks;
+	}
+	
+	public void runGame() {
+		interfaceTerminal.GameStart();
+		
+		//Escolha do modo de jogo:
+		modoDeJogo = interfaceTerminal.escolherModo();
+		criarJogadores();
+		
+		//Escolha dos decks:
+		player1.setDeck(interfaceTerminal.escolherDeck("Jogador 1", listaDecks));
+		player2.setDeck(interfaceTerminal.escolherDeck("Jogador 2", listaDecks));
+		
+		//Escolha das cartas iniciais:
+		player1.substituirCartas(interfaceTerminal.substituirCartasIniciais(player1.getMao()));
+		player2.substituirCartas(interfaceTerminal.substituirCartasIniciais(player2.getMao()));
+		
+		while(!interfaceTerminal.getExitSelected()) {
+			interfaceTerminal.mostrarCampo(Campo campo);
+		}
+		interfaceTerminal.exitGame();
+	}
+	
+	private void criarJogadores() {
+		if(modoDeJogo == ModoDeJogo.HUMANOXHUMANO) {
+			//player1 = new Humano()
+			//player2 = new Humano()
+		}
+		else if(modoDeJogo == ModoDeJogo.HUMANOXCOMPUTADOR) {
+			//player1 = new Humano()
+			//player2 = new Computador()
+		}
+		else {
+			//player1 = new Computador()
+			//player2 = new Computador()
+		}
+	}
+}
