0a2fb7903f281da6834476684f74f556c4691144
andreEidi <andre_eidi_maeda@outlook.com>
2021-07-11 18:28:08 -0300

    tirei o clicoutruefalse...

--- ProjetoMC322/src/com/unicamp/mc322/projeto/campo/InterfaceCampo.java
+++ ProjetoMC322/src/com/unicamp/mc322/projeto/campo/InterfaceCampo.java
@@ -1286,7 +1286,6 @@ public class InterfaceCampo extends javax.swing.JFrame {
 /*												Clique de Botoes	
  * 						  							 */
     private void bntAvancarTurnoActionPerformed(java.awt.event.ActionEvent evt) {
-    	System.out.println("clicou" + aguardandoAcao + realizouAcao);
     	if(realizouAcao == true || rodada.getTipo() == TipoRodada.ESCOLHA_DEFENSORES) {
     		rodada.mudarTipo();
     		aguardandoAcao = false;
